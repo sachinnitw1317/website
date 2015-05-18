f2f8cdb308ef6561a0e32b14797cca7f50d3057d
Sachin Patil <Sachin931350@gmail.com>
2015-05-18 20:30:00 +0530

    complete

---
+++ js/myscript.js
@@ -0,0 +1,8 @@
+function show(id){
+	document.getElementById("learn").style.display="none";
+	document.getElementById("design").style.display="none";
+document.getElementById("publish").style.display="none";
+document.getElementById(id).style.display="block";
+	
+
+}
